f928a6758ee9feb032d6c83f1a4698424bee3497
Takashi Kurokawa <tkurokawa@tkurokawa-MBP.local>
2016-12-04 07:13:04 +0900

    とりあえず1次スプラインgraph embeddingを実装

--- dintegral1.py
+++ dintegral1.py
@@ -74,7 +74,7 @@ def line_integral2d(f, start, end):
 
     elif x0 == x1:
         # (2) y軸に平行な線分．両端を1セルずつはみ出して積分して，端点の外側を引く
-        integral = (1-s0)*f[row0:row1+2, col0].sum() + s0*f[row0:row1+2, col0+1].sum()
+        integral = (1-dx0)*f[row0:row1+2, col0].sum() + dx0*f[row0:row1+2, col0+1].sum()
         integral_extra_B = (1-(1-dy0)**2/2)*((1-dx0)*f[row0, col0] + dx0*f[row0, col0+1]) + dy0**2/2*((1-dx0)*f[row0+1, col0] + dx0*f[row0+1, col0+1])
         integral_extra_T = (1-dy1**2/2)*((1-dx0)*f[row1+1, col0] + dx0*f[row1+1, col0+1]) + (1-dy1)**2/2*((1-dx0)*f[row1, col0] + dx0*f[row1, col0+1])
         return integral - integral_extra_B - integral_extra_T
@@ -198,7 +198,7 @@ def line_integral2d(f, start, end):
 def graph_integral2d(f, G, pos):
     '''
     ２次元標本点上のスカラー場を無向グラフに沿って線積分する関数
-    @param f: np.ndarray((N, M)) ２次元標本点上のスカラー場＝矩形[0, M)x[0, N)上のスカラー場
+    @param f: np.ndarray((N, M)) ２次元標本点上のスカラー場＝矩形[0, M-1)x[0, N-1)上のスカラー場
     @param G: networkx.Graph 0-originの整数をノード名とする無向グラフ
     @param pos: np.ndarray((V, 2)) Gの頂点の座標
     @return 線積分 \int_E(G) f dl
@@ -212,7 +212,17 @@ def graph_integral2d(f, G, pos):
 
 def _cell_grad_integrals(f, rows, cols, theta1, theta2, dx1, dy1, dx2, dy2):
     '''
-    各セルの4種の線積分の和を返す関数
+    各セルにおける4つの線積分の和を返す関数
+    @param f: np.ndarray((N, M)) ２次元標本点上のスカラー場＝矩形[0, M-1)x[0, N-1)上のスカラー場
+    @param rows 各セルの行のインデックス
+    @param cols 各セルの列のインデックス
+    @param theta1 始点から各セルに入る点までの距離の，線分全体に占める割合
+    @param theta2 始点から各セルを出る点までの距離の，線分全体に占める割合
+    @param dx1 各セルに入る点のx変位
+    @param dy1 各セルに入る点のy変位
+    @param dx2 各セルを出る点のx変位
+    @param dy2 各セルを出る点のy変位
+    @return [[L\int(1-\theta)(d\phi/dx(\phi(\theta)))d\theta, L\int(1-\theta)(d\phi/dy(\phi(\theta)))d\theta], [L\int\theta(d\phi/dx(\phi(\theta)))d\theta, L\int\theta(d\phi/dy(\phi(\theta)))d\theta]]
     '''
     ls = np.sqrt((dx2-dx1)**2+(dy2-dy1)**2) # セグメントの長さ
     integrals = np.zeros((2, 2)) # 返却する行列
@@ -222,7 +232,7 @@ def _cell_grad_integrals(f, rows, cols, theta1, theta2, dx1, dy1, dx2, dy2):
     integrals[0, 0] = cells_x0.sum()
 
     # y0に関する微分
-    cells_y0 = ls*((f[rows+1, cols]-f[rows, cols])*((1-dx1)*((1-theta1s)/3+(1-theta2s)/6)+(1-dx2)*((1-theta1s)/6+(1-theta2s)/3)) + (f[rows+1, cols+1]-f[rows, cols+1])*(dx1*((1-theta1)/3+(1-theta2)/6)+dx2*((1-theta1)/6+(1-theta2)/3)))
+    cells_y0 = ls*((f[rows+1, cols]-f[rows, cols])*((1-dx1)*((1-theta1)/3+(1-theta2)/6)+(1-dx2)*((1-theta1)/6+(1-theta2)/3)) + (f[rows+1, cols+1]-f[rows, cols+1])*(dx1*((1-theta1)/3+(1-theta2)/6)+dx2*((1-theta1)/6+(1-theta2)/3)))
     integrals[0, 1] = cells_y0.sum()
 
     # x1に関する微分
@@ -230,7 +240,7 @@ def _cell_grad_integrals(f, rows, cols, theta1, theta2, dx1, dy1, dx2, dy2):
     integrals[1, 0] = cells_x1.sum()
 
     # y1に関する微分
-    cells_y1 = ls*((f[rows+1, cols]-f[rows, cols])*((1-dx1)*(theta1)/3+theta2)/6)+(1-dx2)*(theta1)/6+theta2)/3)) + (f[rows+1, cols+1]-f[rows, cols+1])*(dx1*(theta1/3+theta2/6)+dx2*(theta1/6+theta2/3)))
+    cells_y1 = ls*((f[rows+1, cols]-f[rows, cols])*((1-dx1)*(theta1/3+theta2/6)+(1-dx2)*(theta1/6+theta2/3)) + (f[rows+1, cols+1]-f[rows, cols+1])*(dx1*(theta1/3+theta2/6)+dx2*(theta1/6+theta2/3)))
     integrals[1, 1] = cells_y1.sum()
 
     return integrals
@@ -238,15 +248,14 @@ def _cell_grad_integrals(f, rows, cols, theta1, theta2, dx1, dy1, dx2, dy2):
 def line_integral2d_grad(f, start, end):
     '''
     ２次元標本点上のスカラー場の線分に沿った線積分の，端点の座標に関する勾配ベクトル
-    @param f: np.ndarray((N, M)) ２次元標本点上のスカラー場＝矩形[0, M)x[0, N)上のスカラー場
+    @param f: np.ndarray((N, M)) ２次元標本点上のスカラー場＝矩形[0, M-1)x[0, N-1)上のスカラー場
     @param start: np.ndarray(2) 始点
     @param end: np.ndarray(2) 終点
     @return 線積分の勾配ベクトルの組 \grad_start\int f dl, \grad_end\int f dl
     '''
-    L = np.linalg.norm(end-start) # 線分全体の長さ
-
     # 第一項
 
+    L = np.linalg.norm(end-start) # 線分全体の長さ
     term1st = np.zeros((2, 2))
     term1st[0] = (start-end)/L**2*line_integral2d(f, start, end)
     term1st[1] = -term1st[0]
@@ -255,7 +264,7 @@ def line_integral2d_grad(f, start, end):
 
     if start[1] == end[1]:
         # (1) x軸に平行な場合
-        P_L, P_R = start, end if start[0] < end[0] else end, start
+        P_L, P_R = (start, end) if start[0] < end[0] else (end, start)
         xL, y = P_L.tolist() # 左端点＝始点
         xR, _ = P_R.tolist() # 右端点＝終点
         colL, row = P_L.astype(int).tolist() # 左端点の属するセル
@@ -264,17 +273,17 @@ def line_integral2d_grad(f, start, end):
         dxR, _ = xR-colR, y-row # 右端点の垂直線からの変位
         thetas = (np.arange(colL, colR+2)-xL)/(xR-xL)
 
-        integrals_cover = _cell_grad_integrals(f, np.full(colR-colL, row), np.arange(colL, colR+1), thetas[:-1], thetas[1:], 0, dy, 1, dy)
+        integrals_cover = _cell_grad_integrals(f, np.full(colR-colL+1, row, dtype=int), np.arange(colL, colR+1), thetas[:-1], thetas[1:], 0, dy, 1, dy)
         integrals_extra_L = _cell_grad_integrals(f, np.asarray((row)), np.asarray((colL)), thetas[0], 0, 0, dy, dxL, dy)
-        integrals_extra_R = _cell_grid_integrals(f, np.asarray((row)), np.asarray((colR)), 1, thetas[-1], dxR, dy, 1, dy)
+        integrals_extra_R = _cell_grad_integrals(f, np.asarray((row)), np.asarray((colR)), 1, thetas[-1], dxR, dy, 1, dy)
         integrals = integrals_cover - integrals_extra_L - integrals_extra_R
 
         term2nd = np.zeros((2, 2))
-        term2nd[0], term2nd[1] = integrals[0], integrals[1] if start[0] < end[0] else integrals[1], integrals[0]
+        term2nd[0], term2nd[1] = (integrals[0], integrals[1]) if start[0] < end[0] else (integrals[1], integrals[0])
 
     elif start[0] == end[0]:
         # (2) y軸に平行な場合
-        P_B, P_T = start, end if start[1] < end[1] else end, start
+        P_B, P_T = (start, end) if start[1] < end[1] else (end, start)
         x, yB = P_B.tolist() # 下端点＝始点
         _, yT = P_T.tolist() # 上端点＝終点
         col, rowB = P_B.astype(int).tolist() # 下端点の属するセル
@@ -283,28 +292,26 @@ def line_integral2d_grad(f, start, end):
         _, dyT = x-col, yT-rowT # 上端点の水平線からの変位
         thetas = (np.arange(rowB, rowT+2)-yB)/(yT-yB)
 
-        integrals_cover = _cell_grad_integrals(f, np.arange(rowB, rowT+1), np.full(rowT-rowB, col), thetas[:-1], thetas[1:], dx, 0, dx, 1)
+        integrals_cover = _cell_grad_integrals(f, np.arange(rowB, rowT+1), np.full(rowT-rowB+1, col, dtype=int), thetas[:-1], thetas[1:], dx, 0, dx, 1)
         integrals_extra_B = _cell_grad_integrals(f, np.asarray((rowB)), np.asarray((col)), thetas[0], 0, dx, 0, dx, dyB)
-        integrals_extra_T = _cell_grid_integrals(f, np.asarray((rowT)), np.asarray((col)), 1, thetas[-1], dx, dyT, dx, 1)
+        integrals_extra_T = _cell_grad_integrals(f, np.asarray((rowT)), np.asarray((col)), 1, thetas[-1], dx, dyT, dx, 1)
         integrals = integrals_cover - integrals_extra_B - integrals_extra_T
 
         term2nd = np.zeros((2, 2))
-        term2nd[0], term2nd[1] = integrals[0], integrals[1] if start[1] < end[1] else integrals[1], integrals[0]
+        term2nd[0], term2nd[1] = (integrals[0], integrals[1]) if start[1] < end[1] else (integrals[1], integrals[0])
 
     elif abs(end[0]-start[0]) >= abs(end[1]-start[1]):
         # (3) 寝た線分
-
-        P_L, P_R = start, end if start[0] < end[0] else end, start
+        P_L, P_R = (start, end) if start[0] < end[0] else (end, start)
         xL, yL = P_L.tolist() # 左端点＝始点
         xR, yR = P_R.tolist() # 右端点＝終点
         colL, rowL = P_L.astype(int).tolist() # 左端点の属するセル
         colR, rowR = P_R.astype(int).tolist() # 右端点の属するセル
-        dxL, dyL = xL-colL, y-row # 左端点の垂直線からの変位
-        dxR, dyR = xR-colR, y-row # 右端点の垂直線からの変位
+        dxL, dyL = xL-colL, yL-rowL # 左端点の垂直線からの変位
+        dxR, dyR = xR-colR, yR-rowR # 右端点の垂直線からの変位
 
         dydx = (yR-yL)/(xR-xL) # 傾きdy/dx
         yic = yL-dydx*xL # y切片
-        lx = np.sqrt(1+dydx**2) # x軸方向の単位長さ
 
         rowB, rowT = sorted([rowL, rowR]) # 最下行と最上行
 
@@ -317,17 +324,16 @@ def line_integral2d_grad(f, start, end):
 
         theta1s_nox_H = (cols_nox_H-xL)/(xR-xL) # 「水平線との交点を含まない列の左辺」までの線分全体に占める割合
         theta2s_nox_H = (cols_nox_H+1-xL)/(xR-xL) # 「水平線との交点を含まない列の右辺」までの線分全体に占める割合
-        dx1_nox_H, dx2_nox_H = 0, 1 # 水平線との交点を含まないセルとの交点のx変位
         y1s_nox_H = dydx*cols_nox_H+yic # 「水平線との交点を含まない列の左辺」との交点のy座標
         y2s_nox_H = dydx*(cols_nox_H+1)+yic # 「水平線との交点を含まない列の右辺」との交点のy座標
         rows_nox_H = y1s_nox_H.astype(int) # 水平線との交点を含まない列のセルの行
-        if len(cols_nox_H) > 0 and cols_nox_H[0] == col0:
+        if len(cols_nox_H) > 0 and cols_nox_H[0] == colL:
             # 水平線との交点を含まない列が左端の時，延長線上で水平線をまたぐ場合があるので，補正
-            rows_nox_H[0] = row0
+            rows_nox_H[0] = rowL
         dy1s_nox_H = y1s_nox_H-rows_nox_H # 「水平線との交点を含まない列の左辺」との交点のy変位
         dy2s_nox_H = y2s_nox_H-rows_nox_H # 「水平線との交点を含まない列の右辺」との交点のy変位
 
-        integrals_nox_H = _cell_grad_integrals(f, rows_nox_H, cols_nox_H, theta1s_nox_H, theta2s_nox_H, dx1_nox_H, dy1s_nox_H, dx2_nox_H, dy2s_nox_H)
+        integrals_nox_H = _cell_grad_integrals(f, rows_nox_H, cols_nox_H, theta1s_nox_H, theta2s_nox_H, 0, dy1s_nox_H, 1, dy2s_nox_H)
 
         # (b) 水平線との交点を含む上下のセル
 
@@ -337,7 +343,7 @@ def line_integral2d_grad(f, start, end):
         dxMs_x_H = xs_x_H-cols_x_H # 水平線との交点のx変位
         y1s_x_H = dydx*cols_x_H+yic # 「水平線との交点を含む列の左辺」との交点のy座標
         y2s_x_H = dydx*(cols_x_H+1)+yic # 「水平線との交点を含む列の右辺」との交点のy座標
-        dy1s_x_H = y1s_x_H-y1s_x_H_astype(int) # 「水平線との交点を含む列の左辺」との交点のy変位
+        dy1s_x_H = y1s_x_H-y1s_x_H.astype(int) # 「水平線との交点を含む列の左辺」との交点のy変位
         dy2s_x_H = y2s_x_H-y2s_x_H.astype(int) # 「水平線との交点を含む列の右辺」との交点のy変位
 
         if dydx > 0:
@@ -366,72 +372,88 @@ def line_integral2d_grad(f, start, end):
         integrals = integrals_nox_H + integrals_x_H_B + integrals_x_H_T - integrals_extra_L - integrals_extra_R
 
         term2nd = np.zeros((2, 2))
-        term2nd[0], term2nd[1] = integrals[0], integrals[1] if start[0] < end[0] else integrals[1], integrals[0]
+        term2nd[0], term2nd[1] = (integrals[0], integrals[1]) if start[0] < end[0] else (integrals[1], integrals[0])
 
     else:
         # (4) 立った線分
-        dxdy = (x1-x0)/(y1-y0) # 傾きdx/dy
-        xic = x0-dxdy*y0 # x切片
-        l = np.sqrt(1+dxdy**2) # 単位長さ
+        P_B, P_T = (start, end) if start[1] < end[1] else (end, start)
+        xB, yB = P_B.tolist() # 下端点＝始点
+        xT, yT = P_T.tolist() # 上端点＝終点
+        colB, rowB = P_B.astype(int).tolist() # 下端点の属するセル
+        colT, rowT = P_T.astype(int).tolist() # 上端点の属するセル
+        dxB, dyB = xB-colB, yB-rowB # 下端点の水平線からの変位
+        dxT, dyT = xT-colT, yT-rowT # 上端点の水平線からの変位
 
-        colL, colR = sorted([col0, col1]) # 最左列と最右列
+        dxdy = (xT-xB)/(yT-yB) # 傾きdx/dy
+        xic = xB-dxdy*yB # x切片
+
+        colL, colR = sorted([colB, colT]) # 最左列と最右列
 
         Vs = np.arange(colL+1, colR+1) # 垂直線
         ys_x_V = (Vs-xic)/dxdy # 垂直線との交点のy座標
         rows_x_V = ys_x_V.astype(int) # 垂直線と交点を含む行のインデックス
-        rows_nox_V = np.setdiff1d(np.arange(row0, row1+1), rows_x_V) # 垂直線との交点を含まない行のインデックス
+        rows_nox_V = np.setdiff1d(np.arange(rowB, rowT+1), rows_x_V) # 垂直線との交点を含まない行のインデックス
 
         # (a) 垂直線との交点を含まないセル
 
+        theta1s_nox_V = (rows_nox_V-yB)/(yT-yB) # 「垂直線との交点を含まない行の左辺」までの線分全体に占める割合
+        theta2s_nox_V = (rows_nox_V+1-yB)/(yT-yB) # 「垂直線との交点を含まない行の右辺」までの線分全体に占める割合
         x1s_nox_V = dxdy*rows_nox_V+xic # 「垂直線との交点を含まない行の下辺」との交点のx座標
         x2s_nox_V = dxdy*(rows_nox_V+1)+xic # 「垂直線との交点を含まない行の上辺」との交点のx座標
         cols_nox_V = x1s_nox_V.astype(int) # 垂直線との交点を含まない行のセルの列
-        if len(rows_nox_V) > 0 and rows_nox_V[0] == row0:
+        if len(rows_nox_V) > 0 and rows_nox_V[0] == rowB:
             # 垂直線との交点を含まない行が下端の時，延長線上で垂直線をまたぐ場合があるので，補正
-            cols_nox_V[0] = col0
+            cols_nox_V[0] = colB
         dx1s_nox_V = x1s_nox_V-cols_nox_V # 「垂直線との交点を含まない行の下辺」との交点のx変位
         dx2s_nox_V = x2s_nox_V-cols_nox_V # 「垂直線との交点を含まない行の上辺」との交点のx変位
 
-        cells_nox_V = l*(((1-dx1s_nox_V)/3+(1-dx2s_nox_V)/6)*f[rows_nox_V, cols_nox_V] + ((1-dx1s_nox_V)/6+(1-dx2s_nox_V)/3)*f[rows_nox_V+1, cols_nox_V] + (dx1s_nox_V/3+dx2s_nox_V/6)*f[rows_nox_V, cols_nox_V+1] + (dx1s_nox_V/6+dx2s_nox_V/3)*f[rows_nox_V+1, cols_nox_V]) # 垂直線との交点を含まないセルの線積分
-        integral_nox_V = cells_nox_V.sum()
+        integrals_nox_V = _cell_grad_integrals(f, rows_nox_V, cols_nox_V, theta1s_nox_V, theta2s_nox_V, dx1s_nox_V, 0, dx2s_nox_V, 1)
 
         # (b) 垂直線との交点を含む左右のセル
 
-        dys_x_V = ys_x_V-rows_x_V # 垂直線との交点のy変位
+        theta1s_x_V = (rows_x_V-yB)/(yT-yB) # 「垂直線との交点を含む行の下辺」までの線分全体に占める割合
+        theta2s_x_V = (rows_x_V+1-yB)/(yT-yB) # 「垂直線との交点を含む行の上辺」までの線分全体に占める割合
+        thetaMs_x_V = (Vs-xB)/(xT-xB) # 「垂直線との交点」までの線分全体に占める割合
+        dyMs_x_V = ys_x_V-rows_x_V # 垂直線との交点のy変位
         x1s_x_V = dxdy*rows_x_V+xic # 「垂直線との交点を含む行の下辺」との交点のx座標
         x2s_x_V = dxdy*(rows_x_V+1)+xic # 「垂直線との交点を含む行の上辺」との交点のx座標
-        dx1s_x_V = x1s_x_V-Vs # 「垂直線との交点を含む行の下辺」との交点のx変位
-        dx2s_x_V = x2s_x_V-Vs # 「垂直線との交点を含む行の上辺」との交点のx変位
+        dx1s_x_V = x1s_x_V-x1s_x_V.astype(int) # 「垂直線との交点を含む行の下辺」との交点のx変位
+        dx2s_x_V = x2s_x_V-x2s_x_V.astype(int) # 「垂直線との交点を含む行の上辺」との交点のx変位
 
         if dxdy > 0:
             # 傾き正の場合
-            cell1s_x_V = l*dys_x_V*(((-dx1s_x_V)*(1-dys_x_V)/6+(-dx1s_x_V)/3)*f[rows_x_V, Vs-1] + (-dx1s_x_V)*dys_x_V/6*f[rows_x_V+1, Vs-1] + ((1+dx1s_x_V)*(1-dys_x_V)/6+(1+dx1s_x_V)/3+(1-dys_x_V)/3+1/6)*f[rows_x_V, Vs] + ((1+dx1s_x_V)*dys_x_V/6+dys_x_V/3)*f[rows_x_V+1, Vs]) # 垂直線との交点を含む左のセルの線積分
-            cell2s_x_V = l*(1-dys_x_V)*(((1-dx2s_x_V)*(1-dys_x_V)/6+(1-dys_x_V)/3)*f[rows_x_V, Vs] + ((1-dx2s_x_V)*dys_x_V/6+(1-dx2s_x_V)/3+dys_x_V/3+1/6)*f[rows_x_V+1, Vs] + dx2s_x_V*(1-dys_x_V)/6*f[rows_x_V, Vs+1] + (dx2s_x_V*dys_x_V/6+dx2s_x_V/3)*f[rows_x_V+1, Vs+1]) # 垂直線との交点を含む左のセルの線積分
-            integral_x_V = cell1s_x_V.sum()+cell2s_x_V.sum()
+            integrals_x_V_L = _cell_grad_integrals(f, rows_x_V, Vs-1, theta1s_x_V, thetaMs_x_V, dx1s_x_V, 0, 1, dyMs_x_V) # 左のセル
+
+            integrals_x_V_R = _cell_grad_integrals(f, rows_x_V, Vs, thetaMs_x_V, theta2s_x_V, 0, dyMs_x_V, dx2s_x_V, 1) # 右のセル
         else:
             # 傾き負の場合
-            cell1s_x_V = l*(1-dys_x_V)*((-dx2s_x_V)*(1-dys_x_V)/6*f[rows_x_V, Vs-1] + ((-dx2s_x_V)*dys_x_V/6+(-dx2s_x_V)/3)*f[rows_x_V+1, Vs-1] + ((1+dx2s_x_V)*(1-dys_x_V)/6+(1-dys_x_V)/3)*f[rows_x_V, Vs] + ((1+dx2s_x_V)*dys_x_V/6+(1+dx2s_x_V)/3+dys_x_V/3+1/6)*f[rows_x_V+1, Vs]) # 垂直線との交点を含む左のセルの線積分
-            cell2s_x_V = l*dys_x_V*(((1-dx1s_x_V)*(1-dys_x_V)/6+(1-dx1s_x_V)/3+(1-dys_x_V)/3+1/6)*f[rows_x_V, Vs] + ((1-dx1s_x_V)*dys_x_V/6+dys_x_V/3)*f[rows_x_V+1, Vs] + (dx1s_x_V*(1-dys_x_V)/6+dx1s_x_V/3)*f[rows_x_V, Vs+1] + dx1s_x_V*dys_x_V/6*f[rows_x_V+1, Vs+1]) # 垂直線との交点を含む左のセルの線積分
-            integral_x_V = cell1s_x_V.sum()+cell2s_x_V.sum()
+            integrals_x_V_L = _cell_grad_integrals(f, rows_x_V, Vs-1, thetaMs_x_V, theta2s_x_V, 1, dyMs_x_V, dx2s_x_V, 1) # 左のセル
+
+            integrals_x_V_R = _cell_grad_integrals(f, rows_x_V, Vs, theta1s_x_V, thetaMs_x_V, dx1s_x_V, 0, 0, dyMs_x_V) # 右のセル
 
         # (c) 端点の外側を引く
 
-        x_B = dxdy*row0+xic # 下端点の外側との交点のx座標
-        dx_B = x_B-col0 # 下端点の外側との交点のx変位
-        integral_extra_B = np.rot90(_cell_3_coef(1-dy0, dx0, dx_B), 3).ravel().dot(f[row0:row0+2, col0:col0+2].ravel())
+        thetaBB = (rowB-yB)/(yT-yB) # 下外側端点の線分に占める割合(<0)
+        xBB = dxdy*rowB+xic # 下外側端点のx座標
+        dxBB = xBB-colB # 下外側端点のx変位．0-1に限らない
+        integrals_extra_B = _cell_grad_integrals(f, np.asarray((rowB)), np.asarray((colB)), thetaBB, 0, dxBB, 0, dxB, dyB)
 
-        x_T = dxdy*(row1+1)+xic # 上端点の外側との交点のx座標
-        dx_T = x_T-col1 # 上端点の外側との交点のx変位
-        integral_extra_T = np.rot90(_cell_3_coef(dy1, 1-dx1, 1-dx_T), 1).ravel().dot(f[row1:row1+2, col1:col1+2].ravel())
+        thetaTT = (rowT+1-yB)/(yT-yB) # 上外側端点の線分に占める割合(>1)
+        xTT = dxdy*(rowT+1)+xic # 上外側端点のx座標
+        dxTT = xTT-colT # 上外側端点のx変位．0-1に限らない
+        integrals_extra_T = _cell_grad_integrals(f, np.asarray((rowT)), np.asarray((colT)), 1, thetaTT, dxT, dyT, dxTT, 1)
 
-        return integral_nox_V + integral_x_V - integral_extra_B - integral_extra_T
+        integrals = integrals_nox_V + integrals_x_V_L + integrals_x_V_R - integrals_extra_B - integrals_extra_T
+
+        term2nd = np.zeros((2, 2))
+        term2nd[0], term2nd[1] = (integrals[0], integrals[1]) if start[1] < end[1] else (integrals[1], integrals[0])
 
     return term1st + term2nd
 
 def graph_integral2d_grad(f, G, pos):
     '''
     ２次元標本点上のスカラー場の無向グラフに沿った線積分の，頂点の座標に関する勾配ベクトル
-    @param f: np.ndarray((N, M)) ２次元標本点上のスカラー場＝矩形[0, M)x[0, N)上のスカラー場
+    @param f: np.ndarray((N, M)) ２次元標本点上のスカラー場＝矩形[0, M-1)x[0, N-1)上のスカラー場
     @param G: networkx.Graph 0-originの整数をノード名とする無向グラフ
     @param pos: np.ndarray((V, 2)) Gの頂点の座標
     @return 線積分の勾配ベクトルのnp.array np.array([\grad_P\int_E(G) f dl for P \in V])
@@ -451,9 +473,9 @@ def graph_fit(f, G, pos0, fixed=None, constraints=[]):
     '''
     ２次元標本点上のスカラー場に，無向グラフの埋め込みをフィッティングする関数．
     最適化問題
-    min \int_E(G) f dl s.t. (x_i, y_i) \in [0, M)x[0, N)
+    min \int_E(G) f dl s.t. (x_i, y_i) \in [0, M-1)x[0, N-1)
     を逐次最小二乗法(SLSQP)で解く
-    @param f: np.ndarray((N, M)) ２次元標本点上のスカラー場＝矩形[0, M)x[0, N)上のスカラー場
+    @param f: np.ndarray((N, M)) ２次元標本点上のスカラー場＝矩形[0, M-1)x[0, N-1)上のスカラー場
     @param G: networkx.Graph 0-originの整数をノード名とする無向グラフ
     @param pos0: np.ndarray((V, 2)) Gの頂点の座標の初期値
     @param fixed: np.ndarray((V, 2), dtype=bool) Gの頂点の座標を固定するかどうか
@@ -496,8 +518,8 @@ def graph_fit(f, G, pos0, fixed=None, constraints=[]):
     eps = 1e-6
     bounds = np.zeros((V, 2, 2))
     bounds[:, :, 0] = 0
-    bounds[:, 0, 1] = M-eps
-    bounds[:, 1, 1] = N-eps
+    bounds[:, 0, 1] = M-1-eps
+    bounds[:, 1, 1] = N-1-eps
 
     # その他の制約条件
     cons = []
